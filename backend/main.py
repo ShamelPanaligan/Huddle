20ea68384ef2bbfccf58a01faac7d5b33ba4f528
Shamel Panaligan <PanaliganS@cardiff.ac.uk>
2026-09-20 13:01:08 +0100

    event ideas routes started

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, HTTPException
-from db import get_db_connection, init_db, create_user, get_user_by_email, get_user_by_id
-from schemas import UserRegister, UserOut, UserLogin, TokenResponse
-from auth import hash_password, verify_password, create_access_token, decode_access_token 
-from models import User
+from db import get_db_connection, init_db, create_user, get_user_by_email, get_user_by_id, add_event_idea, list_event_ideas,get_event_idea, match_event_ideas
+from schemas import UserRegister, UserOut, UserLogin, TokenResponse, EventIdeaOut, EventIdeaCreate, MatchRequest
+from auth import hash_password, verify_password, create_access_token, decode_access_token
+from models import User, EventIdea
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends
 
@@ -57,3 +57,34 @@ def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
 @app.get("/me", response_model = UserOut)
 def read_current_user(current_user: User = Depends(get_current_user)):
     return current_user
+
+@app.post("/event-ideas", response_model=EventIdeaOut)
+def create_event_idea(idea_in: EventIdeaCreate, current_user: User = Depends(get_current_user)):
+    conn = get_db_connection()
+    new_idea = EventIdea(creator_id = current_user.id, **idea_in.model_dump())
+    saved = add_event_idea(conn, new_idea)
+    conn.close()
+    return saved
+
+@app.get("/event-ideas", response_model = list[EventIdeaOut])
+def get_event_ideas():
+    conn = get_db_connection()
+    ideas = list_event_ideas(conn)
+    conn.close()
+    return ideas
+
+@app.get("/event-ideas/{idea_id}", response_model = EventIdeaOut)
+def get_event_idea_by_id(idea_id: int):
+    conn = get_db_connection()
+    event = get_event_ideas(conn, idea_id)
+    conn.close()
+
+    if event is None:
+        raise HTTPException(status_code=404, detail="No event with this id")
+
+@app.post("/event-ideas/match", response_model= list[EventIdeaOut])
+def match_ideas(request = MatchRequest):
+    conn = get_db_connection()
+    results = match_event_ideas(conn, **request.model_dump())
+    conn.close()
+    return results
